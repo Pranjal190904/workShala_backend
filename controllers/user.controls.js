3954e5b372631588b566880834b325502e6c3c5c
Prajal190904 <pranjalvyas45@gmail.com>
2023-11-07 17:16:21 +0530

    added change password and forgot password api

--- controllers/user.controls.js
+++ controllers/user.controls.js
@@ -14,7 +14,7 @@ async function userReg(req,res)
         const checkUser=await userModel.findOne({email: email});  //checking if user already exist
         if(checkUser)
         {
-            res.json({message:"User already exist"});
+            res.status(400).json({message:"User already exist"});
             return ;
         }
         const salt=10;
@@ -42,23 +42,23 @@ async function userReg(req,res)
         }); 
     }
     catch(err){
-        res.status(500).json({message:"Server Error",error:err.message}); 
+        res.status(500).json({message:"Server Error"}); 
     }
 }
 
-//function to verify otp
+//function to verify email
 async function verifyEmail(req,res)
 {
     const {email,otp}=req.body;
     const otpVerify=await otpModel.findOne({email: email});
     if(!otpVerify)
     {
-        res.json({message:"email not registered."});
+        res.status(404).json({message:"email not registered."});
         return ;
     }
     if(otp!=otpVerify.otp)
     {
-        res.json({message:"entered otp is incorrect"});
+        res.status(401).json({message:"entered otp is incorrect"});
         return ;
     }
     await userModel.findOneAndUpdate({email: email},{isVerified: true,});
@@ -81,7 +81,7 @@ async function userLogin(req,res)
     {
         const accessToken=await signAccessToken(user.id);
         const refreshToken=await signRefreshToken(user.id);
-        res.json({
+        res.status(200).json({
             accessToken: accessToken,
             refreshToken: refreshToken,
         })
@@ -96,10 +96,83 @@ async function refreshToken(req,res)
     const userId=await verifyRefreshToken(refToken);
     const accessToken=await signAccessToken(userId);
     const refreshToken=await signRefreshToken(userId);
-    res.json({
+    res.status(200).json({
         accessToken:accessToken,
         refreshToken:refreshToken,
     });
 }
 
-module.exports={userReg,userLogin,refreshToken,verifyEmail};
+async function changePassword(req,res)
+{
+    const {email,password,newPassword}=req.body;
+    const user=await userModel.findOne({email:email});
+    if(!email)
+    {
+        res.status(404).json({message:"user not found"});
+        return ;
+    }
+    const matchPassword=bcrypt.compare(password,user.password);
+    if(!matchPassword)
+    {
+        res.status(401).json({message:"entered current password incorrect"});
+        return ;
+    }
+    const hashedPswrd=bcrypt.hash(password,10);
+    user.findOneAndUpdate({email:email},{password:hashedPswrd});
+    res.status(200).json({message:"password updated successfully."})
+}
+
+async function forgotPassword(req,res)
+{
+    const {email}=req.body;
+    const user=await userModel.findOne({email:email});
+    if(!user)
+    {
+        res.status(404).json({message:"user not registered."});
+        return ;
+    }
+    const otp=Math.ceil(Math.random()*8999+1000);
+    const newOtp=new otpModel({
+        email: email,
+        otp: otp,
+    });
+    await newOtp.save();
+    await sendmail(email,otp);
+    res.status(200).json({message:"OTP sent successfully to your registered email."})
+}
+
+//function to verify otp
+async function verifyOtp(req,res)
+{
+    const {email,otp}=req.body;
+    const otpVerify=await otpModel.findOne({email:email});
+    if(!otpVerify)
+    {
+        res.status(404).json({message:"otp expired"});
+        return ;
+    }
+    if(otpVerify.otp!=otp)
+    {
+        res.status(401).json({message:"incorrect otp entered"});
+        return ;
+    }
+    await otpModel.findOneAndDelete({email:email});
+    res.status(200).json({message:"otp verified successfully."});
+}
+
+//function to set new password
+async function setNewPassword(req,res)
+{
+    const {email,newPassword}=req.body;
+    const user=await userModel.findOne({email:email});
+    if(!user)
+    {
+        res.status(404).json({message:"user not found."});
+        return ;
+    }
+    const hashedPswrd=await bcrypt.hash(newPassword,10);
+    await userModel.findOneAndUpdate({email:email},{password:hashedPswrd});
+    res.status(200).json({message:"password reset successful."});
+}
+
+module.exports={userReg,userLogin,refreshToken,verifyEmail,changePassword,forgotPassword,verifyOtp,setNewPassword};
--- routes/user.route.js
+++ routes/user.route.js
@@ -7,5 +7,9 @@ router.post('/register', controls.userReg); //router for user registration
 router.post('/login', controls.userLogin); //router for user login
 router.post('/refreshToken', controls.refreshToken); //router to generate new access and refresh token
 router.post('/verifyEmail',controls.verifyEmail);//router to verify user email
+router.post('/changePassword', controls.changePassword);//router to change password
+router.post('/forgotPassword',controls.forgotPassword);//router to reset password
+router.post('/verifyOtp',controls.verifyOtp);//router to verify otp
+router.post('/setNewPassword',controls.setNewPassword);//router to set new password
 
 module.exports=router;
