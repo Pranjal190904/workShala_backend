992f6a7896a4cb6cfb2c5e2f83e72fbc15ef6f11
Prajal190904 <pranjalvyas45@gmail.com>
2023-11-14 16:12:16 +0530

    fixed cors error

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors({
     origin: "http://localhost:5173",
+    credentials: true,
 }));
 app.use(routerUser);
 app.use(routerCompany);
