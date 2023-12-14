d1dc33a79483d93a0ea2f298db1252fbdce5cd9f
Prajal190904 <pranjalvyas45@gmail.com>
2023-12-14 17:24:05 +0530

    changed type of user phone number

--- models/user.model.js
+++ models/user.model.js
@@ -8,7 +8,7 @@ const userSchema=mongoose.Schema(
             require:true
         },
         phone:{
-            type:Number,
+            type:String,
             require:true
         },
         email:{
